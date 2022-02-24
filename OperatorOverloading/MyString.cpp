c3d02d83650f89f2ae68f625e76e7bf951b508a6
Fidele K.Cyisa <fihacker000@gmail.com>
2022-02-24 16:22:11 +0300

    add files

--- OperatorOverloading/MyString.cpp
+++ OperatorOverloading/MyString.cpp
@@ -26,15 +26,18 @@ MyString::MyString(const MyString &source):str(nullptr){
     str=new char[std::strlen(source.str)+1];
     std::strcpy(str,source.str);
 }
-
+// destructor
 MyString::~MyString(){
     delete[] str;
 }
 
+// display method
 void MyString::display()const{
     std::cout<<str<<":"<<get_length()<<std::endl;
 }
 
+// length getter
 int MyString::get_length() const { return std::strlen(str); }
 
+// string getter
 const char *MyString::get_str() const{ return str;}
