46885b571663de0e6fd2498150a14740676026f6
Fidele K.Cyisa <fihacker000@gmail.com>
2022-04-01 23:15:00 +0300

    folder re-strucure

--- Functions/main.cpp
+++ Functions/main.cpp
@@ -10,16 +10,16 @@ int main(int argc, char const *argv[])
     change_number(numm);    
     cout << numm<<endl;
 
-    /* 
+     
         ///Overloading
     cout << "Overloading program example" << endl
          << endl;
     cout << "greet() function with only first name" << endl;
     greet("Fidele");
-    cout << "greet() function with both firstname and lastname" << endl; // output : Hello Fidele
+    cout << "greet() function with both firstname and lastname" << endl; // output : Helloooo Fidele
     greet("Fidele", "Kirezi");                                           // output : Fidele Kirezi
     cout << endl;
-    */
+    
 
     return 0;
 }
--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -9,18 +9,15 @@
 
 using namespace std;
 
-
-
-
 int main(int argc, char const *argv[])
 {
 
-    //accessing_pointer(); 
-    //allocate_memory();
-    //allocate_memory_array();
-    //array_and_pointer_relationships();
+    accessing_pointer(); 
+    allocate_memory();
+    allocate_memory_array();
+    array_and_pointer_relationships();
 
-    /*
+    
     int scores[] {100,50,30,40,60};
 
     int *score_ptr {scores};
@@ -30,27 +27,27 @@ int main(int argc, char const *argv[])
         cout<<*score_ptr<<endl;
         score_ptr++;
     }
-    */
+    
 
-    /*
+    
     int num {10};
     cout<<num<<endl;
     square_root(&num);
     cout<<num<<endl;
-    */
+    
 
 
 
-    /*
+    
     auto names = create_array(10,"Fidele Ni Umuti");
 
     for (size_t i = 0; i < 10; i++)
     {
         cout<<names[i]<<endl;
-    }*/
+    }
     
 
-    /* test references 
+     
     vector<string> programmings {"Python","c++","javascripts"};
 
     for(auto programming:programmings)
@@ -61,9 +58,9 @@ int main(int argc, char const *argv[])
 
     for (auto programming : programmings)
         cout<<programming<<endl;
-    */
+    
 
-    /*
+    
         ///test the challenge from challenge.hpp dynamic allocation using pointers
 
     int arr1[] { 1, 2, 3, 4, 5 };
@@ -72,7 +69,7 @@ int main(int argc, char const *argv[])
     int *results = apply_all(arr1,5,arr2,3);
 
     print(results,15);
-    */
+    
    
     return 0;
 }
