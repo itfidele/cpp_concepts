8f2077fa8ae344b021c7bf3b93a62ba14ca4a040
Fidele K.Cyisa <fihacker000@gmail.com>
2022-08-02 19:03:28 +0300

    updates

--- Inherintance/Derived.cpp
+++ Inherintance/Derived.cpp
@@ -1,11 +1,11 @@
 #include "Derived.hpp"
 
 
-Derived::Derived():double_value(0){
+Derived::Derived():Base(),double_value(0){
     cout<<"no-args Derived contructor called"<<endl;
 }
 
-Derived::Derived(int x):double_value(x){
+Derived::Derived(int x):Base(x),double_value(x*2){
     cout<<"Derived(int) arg contructor called"<<endl;
 }
 
--- Inherintance/main.cpp
+++ Inherintance/main.cpp
@@ -6,50 +6,52 @@
 
 
 int main(){
-    Account fidele_account {};
-    fidele_account.deposit(3000.0);
-    fidele_account.withdraw(500.0);
+    // Account fidele_account {};
+    // fidele_account.deposit(3000.0);
+    // fidele_account.withdraw(500.0);
 
-    cout<<endl;
+    // cout<<endl;
     
-    Account *destin_account {nullptr};
-    destin_account= new Account();
-    destin_account->deposit(4000.0);
-    destin_account->withdraw(250.0);
-    delete destin_account;
+    // Account *destin_account {nullptr};
+    // destin_account= new Account();
+    // destin_account->deposit(4000.0);
+    // destin_account->withdraw(250.0);
+    // delete destin_account;
 
-    cout<<endl;
+    // cout<<endl;
 
-    cout<<"============================= Savings account now ==========================="<<endl;
+    // cout<<"============================= Savings account now ==========================="<<endl;
 
-    Savings_Account fidele_saving {};
-    fidele_saving.deposit(500.0);
-    fidele_saving.withdraw(20.0);
+    // Savings_Account fidele_saving {};
+    // fidele_saving.deposit(500.0);
+    // fidele_saving.withdraw(20.0);
 
-    cout<<endl;
+    // cout<<endl;
 
-    Savings_Account *destin_saving {nullptr};
-    destin_saving = new Savings_Account();
-    destin_saving->deposit(5000.0);
-    destin_saving->withdraw(400.0);
-    delete destin_saving;
+    // Savings_Account *destin_saving {nullptr};
+    // destin_saving = new Savings_Account();
+    // destin_saving->deposit(5000.0);
+    // destin_saving->withdraw(400.0);
+    // delete destin_saving;
 
-    cout<<"============== Base memeber access from Base object========================"<<endl;
-    Base base;
-    base.a=100; //OK
-    //base.b=200;
-    //base.c=200; //Compiler Error
+    // cout<<"============== Base memeber access from Base object========================"<<endl;
+    // Base base;
+    // base.a=100; //OK
+    // //base.b=200;
+    // //base.c=200; //Compiler Error
     
-    cout<<"============== Base memeber access from Derived object======================"<<endl;
-    Derived d;
-    d.a=100; // Ok
-    //d.b=200; //Error
-    //d.c=300; // Error
+    // cout<<"============== Base memeber access from Derived object======================"<<endl;
+    // Derived d;
+    // d.a=100; // Ok
+    // //d.b=200; //Error
+    // //d.c=300; // Error
 
-    Base bee;
-    Base b{100};
-    Derived de;
-    Derived be{100};
+    // Base bee;
+    // Base b{100};
+    // Derived de;
+    // Derived be{100};
+
+    Derived d{10009};
 
     return 0;
 }
